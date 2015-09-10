fc9d5c5980a44053e37df21e90ec822c09cb09eb
SGE04 <siby.george@bskyb.com>
2015-09-10 14:39:13 +0100

    SG : 318 : ReceiveTimeout added to the actor

--- src/main/scala/com/example/Application316_318.scala
+++ src/main/scala/com/example/Application316_318.scala
@@ -1,15 +1,20 @@
 package com.example
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor._
 import akka.event.Logging
 
+import scala.concurrent.duration.Duration.Undefined
 import scala.concurrent.duration._
 
 class MyActor extends Actor {
   val log = Logging(context.system, this)
 
+  context.setReceiveTimeout(30 milliseconds)
+
   override def receive: Receive = {
     case "test" => log.info("received test")
+    case ReceiveTimeout => context.setReceiveTimeout(Undefined)
+      throw new RuntimeException("Receive time out")
     case _ => log.info("unknown msg")
   }
 }
@@ -18,7 +23,7 @@ object MyActor {
   def props() = Props(new MyActor())
 }
 
-object Application316 extends App {
+object Application316_318 extends App {
   val system = ActorSystem("316")
   val myActor: ActorRef = system.actorOf(MyActor.props(), "myActor")
 
